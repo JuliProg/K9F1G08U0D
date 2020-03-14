cf810f1130754b5e464f297aa84adffa7e8ade95
Taras <juliprog1110@gmail.com>
2020-03-14 19:16:38 +0200

    Add IDInterpreted class

--- K9F1G08U0D/ChipAssembly.cs
+++ K9F1G08U0D/ChipAssembly.cs
@@ -244,9 +244,9 @@ namespace K9F1G08U0D
         private object obj1;
 
 
-        
-        [Export("ID Register Interpreted", typeof(Register))]    //  to ID Register
-        [Import("K9F1G08U0D_IDInterpreted", typeof(Register))]   // from [some].dll
+
+        [Export("ID Register Interpreted", typeof(Func<Register, string>))]    //  to ID Register
+        [Import("K9F1G08U0D_IDInterpreted", typeof(Func<Register, string>))]                 // from here
         private object obj2;
 
         #endregion
@@ -281,6 +281,176 @@ namespace K9F1G08U0D
 
     }
 
-   
+    public class IDInterpreted
+    {
+        [Export("K9F1G08U0D_IDInterpreted", typeof(Func<Register, string>))]
+        public string Interpreted(Register register)
+        {
+            string messsage = "1st Byte    Maker Code = " + BitConverter.ToString(register.GetContent(), 0, 1) + Environment.NewLine;
+            messsage += "2st Byte    Device Code = " + BitConverter.ToString(register.GetContent(), 1, 1) + Environment.NewLine;
+            messsage += "3rd ID Data = " + BitConverter.ToString(register.GetContent(), 2, 1) + Environment.NewLine;
+            messsage += Encoding(register.GetContent()[2], 2) + Environment.NewLine;
+            messsage += "4rd ID Data = " + BitConverter.ToString(register.GetContent(), 3, 1) + Environment.NewLine;
+            messsage += Encoding(register.GetContent()[3], 3) + Environment.NewLine;
+            messsage += "5rd ID Data = " + BitConverter.ToString(register.GetContent(), 4, 1) + Environment.NewLine;
+            messsage += Encoding(register.GetContent()[4], 4) + Environment.NewLine;
+
+            return messsage;
+        }
+        private string Encoding(byte bt, int pos)
+        {
+            string str_result = String.Empty;
+
+            var IO = new System.Collections.BitArray(new[] { bt });
+
+            switch (pos)
+            {
+                case 2:
+                    str_result += " Internal Chip Number = ";
+                    if (IO[1] == false && IO[0] == false)
+                        str_result += "1";
+                    if (IO[1] == false && IO[0] == true)
+                        str_result += "2";
+                    if (IO[1] == true && IO[0] == false)
+                        str_result += "4";
+                    if (IO[1] == true && IO[0] == true)
+                        str_result += "8";
+                    str_result += Environment.NewLine;
+
+
+                    str_result += " Cell Type = ";
+                    if (IO[3] == false && IO[2] == false)
+                        str_result += "2 Level Cell";
+                    if (IO[3] == false && IO[2] == true)
+                        str_result += "4 Level Cell";
+                    if (IO[3] == true && IO[2] == false)
+                        str_result += "8 Level Cell";
+                    if (IO[3] == true && IO[2] == true)
+                        str_result += "16 Level Cell";
+                    str_result += Environment.NewLine;
+
+
+                    str_result += " Number of Simultaneously Programmed Pages = ";
+                    if (IO[5] == false && IO[4] == false)
+                        str_result += "1";
+                    if (IO[5] == false && IO[4] == true)
+                        str_result += "2";
+                    if (IO[5] == true && IO[4] == false)
+                        str_result += "4";
+                    if (IO[5] == true && IO[4] == true)
+                        str_result += "8";
+                    str_result += Environment.NewLine;
+
+
+                    str_result += " Interleave Program Between multiple chips = ";
+                    if (IO[6] == false)
+                        str_result += "Not Support";
+                    if (IO[6] == true)
+                        str_result += "Support";
+                    str_result += Environment.NewLine;
+
+                    str_result += " Cache Program = ";
+                    if (IO[7] == false)
+                        str_result += "Not Support";
+                    if (IO[7] == true)
+                        str_result += "Support";
+                    str_result += Environment.NewLine;
+                    break;
+
+                case 3:
+
+                    str_result += " Page Size (w/o redundant area ) = ";
+                    if (IO[1] == false && IO[0] == false)
+                        str_result += "1KB";
+                    if (IO[1] == false && IO[0] == true)
+                        str_result += "2KB";
+                    if (IO[1] == true && IO[0] == false)
+                        str_result += "4KB";
+                    if (IO[1] == true && IO[0] == true)
+                        str_result += "8KB";
+                    str_result += Environment.NewLine;
+
+
+                    str_result += " Block Size (w/o redundant area ) = ";
+                    if (IO[5] == false && IO[4] == false)
+                        str_result += "64KB";
+                    if (IO[5] == false && IO[4] == true)
+                        str_result += "128KB";
+                    if (IO[5] == true && IO[4] == false)
+                        str_result += "256KB";
+                    if (IO[5] == true && IO[4] == true)
+                        str_result += "512KB";
+                    str_result += Environment.NewLine;
+
+
+                    str_result += " Redundant Area Size ( byte/512byte) = ";
+                    if (IO[2] == false)
+                        str_result += "8";
+                    if (IO[2] == true)
+                        str_result += "16";
+                    str_result += Environment.NewLine;
+
+
+                    str_result += " Organization = ";
+                    if (IO[6] == false)
+                        str_result += "x8";
+                    if (IO[6] == true)
+                        str_result += "x16";
+                    str_result += Environment.NewLine;
+
+                    str_result += " Serial Access Minimum = ";
+                    if (IO[7] == false && IO[3] == false)
+                        str_result += "50ns/30ns";
+                    if (IO[7] == true && IO[3] == false)
+                        str_result += "25ns";
+                    if (IO[7] == false && IO[3] == true)
+                        str_result += "Reserved";
+                    if (IO[7] == true && IO[3] == true)
+                        str_result += "Reserved";
+                    str_result += Environment.NewLine;
+                    break;
+
+                case 4:
+
+                    str_result += " Plane Number = ";
+                    if (IO[3] == false && IO[2] == false)
+                        str_result += "1";
+                    if (IO[3] == false && IO[2] == true)
+                        str_result += "2";
+                    if (IO[3] == true && IO[2] == false)
+                        str_result += "4";
+                    if (IO[3] == true && IO[2] == true)
+                        str_result += "8";
+                    str_result += Environment.NewLine;
+
+
+                    str_result += " Plane Size (w/o redundant area ) = ";
+                    if (IO[6] == false && IO[5] == false && IO[4] == false)
+                        str_result += "64Mb";
+                    if (IO[6] == false && IO[5] == false && IO[4] == true)
+                        str_result += "128Mb";
+                    if (IO[6] == false && IO[5] == true && IO[4] == false)
+                        str_result += "256Mb";
+                    if (IO[6] == false && IO[5] == true && IO[4] == true)
+                        str_result += "512Mb";
+                    if (IO[6] == true && IO[5] == false && IO[4] == false)
+                        str_result += "1Gb";
+                    if (IO[6] == true && IO[5] == false && IO[4] == true)
+                        str_result += "2Gb";
+                    if (IO[6] == true && IO[5] == true && IO[4] == false)
+                        str_result += "4Gb";
+                    if (IO[6] == true && IO[5] == true && IO[4] == true)
+                        str_result += "8Gb";
+                    str_result += Environment.NewLine;
+
+
+                    break;
+            }
+            return str_result;
+        }
+    }
+
+
+
     #endregion
 }
